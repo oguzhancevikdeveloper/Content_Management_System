ca976bb68942e02992de77ce54fad95761eaaca0
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-11-21 16:03:28 +0300

    Exception yapısı eklendi ve özelleştirilmiş CustomBaseController oluşturuldu

---
+++ ContentManagementSystem/shared/CMS.Shared/Configuration/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using CMS.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Shared.Configuration;
+
+public class CustomBaseController : ControllerBase
+{
+    public IActionResult ActionResultInstance<T>(Response<T> response) where T : class
+    {
+        return new ObjectResult(response)
+        {
+            StatusCode = response.StatusCode,
+        };
+    }
+
+}
---
+++ ContentManagementSystem/shared/CMS.Shared/Exceptions/CustomException.cs
@@ -0,0 +1,8 @@
+﻿namespace CMS.Shared.Exceptions;
+
+public class CustomException : Exception
+{
+    public CustomException() : base() { }
+    public CustomException(string message) : base(message) { }
+    public CustomException(string message, Exception innerException) : base(message, innerException) { }
+}
